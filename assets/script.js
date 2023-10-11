b05bc2ffe97f5c15fde61ac72ed7964342ad7c50
Mierven <>
2023-10-11 16:57:34 +0000

    making game done

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,33 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let player = document.getElementById("enemy")
-    document.addEventListener("keydown", function(event){
-        if (event.KeyCode === 37) (// Left arrow key)
-        movePlayer(-10);
-        
+document.addEventListener("DOMContentLoaded", function () {
+    var player = document.getElementById("player");
+    var enemy = document.getElementById("enemy");
+
+    document.addEventListener("keydown", function (event) {
+        if (event.keyCode === 37) { // Left arrow key)
+            movePlayer(-10);
+
+        } else if (event.keyCode === 39) // Right arrow key)
+            movePlayer(10);
+
+
+    });
+
+    function movePlayer(amount) {
+        var currentPosition = parseInt(player.style.left);
+        player.style.left = currentPosition + amount + "px";
+        checkCollision();
+    }
+    function movePlayer(amount) {
+        var playerRect = player.getBoundingClientRect();
+        var enemyrRect = enemy.getBoundingClientRect();
+
+        if (playerRect.right >= enemyRect.left && playerRect.left <= enemyRect.right) {
+            alert("Collision detected!");
+
+        }
+    }
+
+});
+
+
+
